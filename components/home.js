fbe649bc47afb230644db8017dc898bcfad52c7f
Akansha Bajpai <akanshabajpai0307@gmail.com>
2021-03-08 16:57:12 +0530

    Changes made to the homepage

--- components/home.js
+++ components/home.js
@@ -3,27 +3,16 @@ import { Name, Tagline, Description, Time, Venue } from '../lib/constants'
 
 export default function Home(){
     return(
-        <div className={styles.container}>
-            <div style={{ minWidth: '300px' }}>
-                <img
-                    // alt={speaker.name}
-                    // title={speaker.name}
-                    src='/speakers/akansha.png'
-                    className={styles.image}
-                    loading="lazy"
-                    height={300}
-                    width={300}
-                />
-            </div>
+        <div>
             <div className={styles['speaker-details']}>
                 <div>
-                    <h2 className={styles.name}>{Name}</h2>
-                    <p className={styles.title}>
+                    <h2 className={styles.eventname}>{Name}</h2>
+                    <p className={styles.eventtitle}>
                         {Tagline}
                     </p>
-                    <h2 className={styles['bio-header']}><strong>Description</strong></h2>
-                    <p className={styles.bio}>{Description}</p>
-                    <h2 className={styles['bio-header']}><strong>{Time} | {Venue}</strong></h2>
+
+                    <p className={styles.description}>{Description}</p>
+                    <h2 className={styles['description-header']}><strong>{Time} | {Venue}</strong></h2>
                 </div>
             </div>
         </div>
