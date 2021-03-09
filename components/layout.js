65028221bb46ea1b6b5c4c4ffe92b1edf6573363
Devansh Srivastav <devansh.srivastav@gmail.com>
2021-03-09 23:08:04 +0530

    Inferno

--- components/layout.js
+++ components/layout.js
@@ -1,15 +1,10 @@
 import NavBar from "./navbar"
 import Footer from "./footer";
-import Head from 'next/head'
-import { Name } from '../lib/constants'
+
 
 const Layout = ({ children }) => {
     return (
         <>
-            <Head>
-                <title>{Name}</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
             <NavBar />
             {children}
             <Footer />
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import Layout from '../components/layout'
 import '../styles/globals.css'
+import Head from 'next/head'
+import { Name } from '../lib/constants'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>{Name}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
