3e70737d874f83b2d7b9fc210c0b552f371c1863
Devansh Srivastav <devansh.srivastav@gmail.com>
2021-03-09 23:05:15 +0530

    Inferno

--- components/layout.js
+++ components/layout.js
@@ -1,19 +1,7 @@
 import NavBar from "./navbar"
-import { theme, ThemeProvider, CSSReset } from "@chakra-ui/react";
 import Footer from "./footer";
 import Head from 'next/head'
 import { Name } from '../lib/constants'
-
-// const breakpoints = ["360px", "768px", "1024px", "1440px"];
-// breakpoints.sm = breakpoints[0];
-// breakpoints.md = breakpoints[1];
-// breakpoints.lg = breakpoints[2];
-// breakpoints.xl = breakpoints[3];
-
-// const newTheme = {
-//     ...theme,
-//     breakpoints
-// };
 
 const Layout = ({ children }) => {
     return (
